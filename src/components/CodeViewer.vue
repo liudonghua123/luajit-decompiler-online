8864283b99f59764db616b6da6d9cdc49eed7a61
liudonghua123 <liudonghua123@gmail.com>
2026-05-28 13:18:27 +0800

    fix monaco editor load failure, Cannot use import statement outside a module

--- src/components/CodeViewer.vue
+++ src/components/CodeViewer.vue
@@ -14,18 +14,18 @@ let editor: monaco.editor.IStandaloneCodeEditor | null = null
 window.MonacoEnvironment = {
   getWorker(_, label) {
     if (label === 'json') {
-      return new Worker(new URL('monaco-editor/esm/vs/language/json/json.worker.js?worker', import.meta.url));
+      return new Worker(new URL('monaco-editor/esm/vs/language/json/json.worker.js?worker', import.meta.url), { type: 'module' });
     }
     if (label === 'css' || label === 'scss' || label === 'less') {
-      return new Worker(new URL('monaco-editor/esm/vs/language/css/css.worker.js?worker', import.meta.url));
+      return new Worker(new URL('monaco-editor/esm/vs/language/css/css.worker.js?worker', import.meta.url), { type: 'module' });
     }
     if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return new Worker(new URL('monaco-editor/esm/vs/language/html/html.worker.js?worker', import.meta.url));
+      return new Worker(new URL('monaco-editor/esm/vs/language/html/html.worker.js?worker', import.meta.url), { type: 'module' });
     }
     if (label === 'typescript' || label === 'javascript') {
-      return new Worker(new URL('monaco-editor/esm/vs/language/typescript/ts.worker.js?worker', import.meta.url));
+      return new Worker(new URL('monaco-editor/esm/vs/language/typescript/ts.worker.js?worker', import.meta.url), { type: 'module' });
     }
-    return new Worker(new URL('monaco-editor/esm/vs/editor/editor.worker.js?worker', import.meta.url));
+    return new Worker(new URL('monaco-editor/esm/vs/editor/editor.worker.js?worker', import.meta.url), { type: 'module' });
   },
 };
 
